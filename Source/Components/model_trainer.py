be556f9e7499cde1ba1830e3aedc8ac2cc4fd925
THOWFI <1404thow@gmail.com>
2025-08-14 15:09:41 +0530

    HyperTunned Model Trained with Pickled

--- Source/Components/model_trainer.py
+++ Source/Components/model_trainer.py
@@ -51,7 +51,77 @@ class ModelTrainer:
                 "Gradient Boosting" : GradientBoostingRegressor()
             }
 
-            model_report:dict=eval_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,models=models)
+            
+            params= {
+
+                "Random Forest Regressor" : {
+                    "max_depth": [5, 8, 10, 15, None],
+                    "max_features": [5, 7, "auto", "sqrt"],
+                    "min_samples_split": [2, 5, 8, 15, 20],
+                    "min_samples_leaf": [1, 2, 4],
+                    "n_estimators": [100, 200, 500, 1000]
+                },
+                "XGBRegressor" : {
+                    "learning_rate": [0.3, 0.1, 0.05, 0.01],
+                    "max_depth": [3, 5, 8, 12],
+                    "n_estimators": [100, 200, 500],
+                    "colsample_bytree": [0.3, 0.5, 0.8, 1],
+                    "subsample": [0.6, 0.8, 1]
+                },
+                "CatBoost Regressor" : {
+                    "iterations": [200, 500, 1000],
+                    "depth": [4, 6, 8, 10],
+                    "learning_rate": [0.3, 0.1, 0.05, 0.01],
+                    "l2_leaf_reg": [1, 3, 5, 7, 9]
+                },
+                "Support Vector Regressor" : {
+                    "kernel": ["linear", "poly", "rbf", "sigmoid"],
+                    "C": [0.1, 1, 10, 100],
+                    "epsilon": [0.01, 0.1, 0.2, 0.5],
+                    "gamma": ["scale", "auto"]
+                },
+                "K-Neighbors Regressor" : {
+                    "n_neighbors": [3, 5, 7, 9, 15],
+                    "weights": ["uniform", "distance"],
+                    "metric": ["euclidean", "manhattan", "minkowski"]
+                },
+                "Decision Tree Regressor" : {
+                    "max_depth": [3, 5, 8, 12, None],
+                    "min_samples_split": [2, 5, 10, 15],
+                    "min_samples_leaf": [1, 2, 4, 6],
+                    "max_features": [None, "sqrt", "log2"]
+                },
+                "Lasso" : {
+                    "alpha": [0.001, 0.01, 0.1, 1, 5, 10],
+                    "max_iter": [500, 1000, 5000]
+                },
+                "Ridge" : {
+                    "alpha": [0.001, 0.01, 0.1, 1, 5, 10, 50],
+                    "max_iter": [500, 1000, 5000]
+                },
+                "LinearRegression" : {
+                    "fit_intercept": [True, False],
+                    "positive": [True, False]
+                },
+                "Ada Boost Regressor" : {
+                    "n_estimators": [50, 100, 200, 500],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.5, 1],
+                    "loss": ["linear", "square", "exponential"]
+                },
+                "Gradient Boosting" : {
+                    "n_estimators": [100, 200, 500],
+                    "learning_rate": [0.3, 0.1, 0.05, 0.01],
+                    "max_depth": [3, 5, 8],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "subsample": [0.6, 0.8, 1.0],
+                    "max_features": [None, "sqrt", "log2"],
+                    "loss": ["squared_error", "absolute_error", "huber"]
+                }
+            }
+
+
+            model_report:dict=eval_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,models=models,param=params)
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
--- Source/utils.py
+++ Source/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from sklearn.metrics import r2_score
 from Source.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -21,11 +22,16 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def eval_model(X_train,y_train,X_test,y_test,models):
+def eval_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3,n_jobs=-1)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train)
             #Making Predictions
             y_train_pred=model.predict(X_train)
